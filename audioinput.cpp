5c649a66673e3cf93a9010896631bbe85b364173
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-10-19 09:46:28 +0330

    add .pro to git ignore

---
+++ audioinput.cpp
@@ -0,0 +1,3 @@
+#include "audioinput.h"
+
+AudioInput::AudioInput() {}
---
+++ audioinput.h
@@ -0,0 +1,12 @@
+#ifndef AUDIOINPUT_H
+#define AUDIOINPUT_H
+
+#include <QIODevice>
+
+class AudioInput : public QIODevice
+{
+public:
+    AudioInput();
+};
+
+#endif // AUDIOINPUT_H
