3deed3757677a2d7c4aa5ba1a7ba90a49372b5c9
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2024-10-19 17:47:47 +0330

    Change server.js to distinguish offer and answer

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const io = require('socket.io')(3000);
 
 const clients = {};
 
+function handle_sdp_messages(socket, data, type){
+    console.log("new offer sdp msg")
+    const targetClientId = data.targetClientId;
+    const sdp = data.sdp;
+
+    if (clients[targetClientId]) {
+        clients[targetClientId].emit(type + '_sdp', {
+            from: socket.id,
+            sdp: sdp
+        });
+    } else {
+        socket.emit('error', { message: 'Target client not connected' });
+    }
+}
+
 io.on('connection', (socket) => {
     console.log('New client connected:', socket.id);
 
@@ -9,19 +24,9 @@ io.on('connection', (socket) => {
 
     socket.emit('your_id', socket.id);
 
-    socket.on('send_sdp', (data) => {
-        const targetClientId = data.targetClientId;
-        const sdp = data.sdp;
+    socket.on('offer_sdp', (data) => handle_sdp_messages(socket, data, "offer"));
 
-        if (clients[targetClientId]) {
-            clients[targetClientId].emit('send_sdp', {
-                from: socket.id,
-                sdp: sdp
-            });
-        } else {
-            socket.emit('error', { message: 'Target client not connected' });
-        }
-    });
+    socket.on('answer_sdp', (data) => handle_sdp_messages(socket, data, "answer"));
 
     socket.on('disconnect', () => {
         console.log('Client disconnected:', socket.id);
