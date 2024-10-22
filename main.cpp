a53e9dfc0e3ec13151a2f0623e585f340f1dbf0b
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-10-22 11:32:13 +0330

    connect signal and slots of sending and receiving sdp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@
 #include "audioinput.h"
 #include "audiooutput.h"
 #include "client.h"
+#include <webrtc.h>
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
-    AudioInput audioInput;
-    AudioOutput audioOutput;
+    // AudioInput audioInput;
+    // AudioOutput audioOutput;
     Client client;
-    QObject::connect(&audioInput, &AudioInput::AudioIsReady, &audioOutput, &AudioOutput::addData);
+    WebRTC webrtc;
+    // QObject::connect(&audioInput, &AudioInput::AudioIsReady, &audioOutput, &AudioOutput::addData);
+
+    QObject::connect(&client, &Client::localIdIsSet, &webrtc, &WebRTC::init);
+    QObject::connect(&webrtc, &WebRTC::offerIsReady, &client, &Client::sendOffer);
+    QObject::connect(&webrtc, &WebRTC::answerIsReady, &client, &Client::sendAnswer);
+    QObject::connect(&client, &Client::newSdpReceived, &webrtc, &WebRTC::setRemoteDescription);
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("client", &client);
