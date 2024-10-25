596a9c16b720620a8221a3cb36e0051091a87076
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-10-25 10:52:08 +0330

    make server logs more readable

--- server.js
+++ server.js
@@ -2,11 +2,36 @@ const io = require('socket.io')(3000);
 
 const clients = {};
 
-function handle_sdp_messages(socket, data, type){
-    console.log("new sdp msg" + data)
+// ANSI escape codes for color
+const COLORS = {
+    reset: "\x1b[0m",
+    blue: "\x1b[34m",
+    green: "\x1b[32m",
+    cyan: "\x1b[36m",
+    magenta: "\x1b[35m",
+    yellow: "\x1b[33m",
+    red: "\x1b[31m",
+};
+
+function colorText(text, color) {
+    return `${color}${text}${COLORS.reset}`;
+}
+
+function truncateString(str, maxLength = 100) {
+    return str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
+}
+
+function handle_sdp_messages(socket, data, type) {
+    console.log(colorText(`Received ${type} SDP message`, COLORS.blue));
     data = JSON.parse(data);
     const targetClientId = data.targetClientId;
     const sdp = data.sdp;
+
+    stringSdp = JSON.parse(sdp);
+    console.log(colorText(`Target Client ID: ${truncateString(targetClientId)}`, COLORS.green));
+    console.log(colorText(`SDP type: ${truncateString(stringSdp.type)}`, COLORS.cyan));
+    console.log(colorText(`SDP sdp: ${truncateString(stringSdp.sdp)}`, COLORS.cyan));
+
     if (clients[targetClientId]) {
         clients[targetClientId].emit(type + '_sdp', {
             from: socket.id,
@@ -14,15 +39,21 @@ function handle_sdp_messages(socket, data, type){
         });
     } else {
         socket.emit('error', { message: 'Target client not connected' });
+        console.log(colorText(`Error: Target client ${targetClientId} not connected`, COLORS.red));
     }
 }
 
-function handle_ice_messages(socket, data){
-    console.log("new ice candidate msg " + data );
+function handle_ice_messages(socket, data) {
+    console.log(colorText("Received ICE candidate message", COLORS.blue));
     data = JSON.parse(data);
     const targetClientId = data.targetClientId;
     const candidate = data.candidate;
     const mid = data.mid;
+
+    console.log(colorText(`Target Client ID: ${truncateString(targetClientId)}`, COLORS.green));
+    console.log(colorText(`ICE Candidate: ${truncateString(candidate)}`, COLORS.magenta));
+    console.log(colorText(`MID: ${truncateString(mid)}`, COLORS.yellow));
+
     if (clients[targetClientId]) {
         clients[targetClientId].emit('send_ice', {
             from: socket.id,
@@ -31,11 +62,12 @@ function handle_ice_messages(socket, data){
         });
     } else {
         socket.emit('error', { message: 'Target client not connected' });
+        console.log(colorText(`Error: Target client ${targetClientId} not connected`, COLORS.red));
     }
 }
 
 io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
+    console.log(colorText('New client connected:', COLORS.green), colorText(socket.id, COLORS.yellow));
 
     clients[socket.id] = socket;
 
@@ -48,9 +80,7 @@ io.on('connection', (socket) => {
     socket.on("send_ice", (data) => handle_ice_messages(socket, data));
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
+        console.log(colorText('Client disconnected:', COLORS.red), colorText(socket.id, COLORS.yellow));
         delete clients[socket.id];
     });
-
-    
 });
